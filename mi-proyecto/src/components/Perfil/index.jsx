d42637e6013f231df662f7880c1a60a5ac0bfcfe
LUDMILA UMA GRÜN <49379260@ort>
2026-05-12 10:57:22 -0300

    Perfil creado

--- mi-proyecto/src/components/Perfil/index.jsx
+++ mi-proyecto/src/components/Perfil/index.jsx
@@ -3,8 +3,48 @@ import "./Perfil.css";
 function Perfil() {
   return (
     <aside className="perfil">
-      <h3>@catlover</h3>
-      <p>Amante de los gatos 🐱</p>
+
+      <div className="perfil-header">
+
+        <img
+          src="https://i.pravatar.cc/150?img=32"
+          alt="perfil"
+          className="perfil-imagen"
+        />
+
+        <div>
+          <h3>ludmi.cats</h3>
+
+          <p className="bio">
+            Amante de los gatos 🐱
+          </p>
+        </div>
+
+      </div>
+
+      <div className="perfil-stats">
+
+        <div>
+          <strong>48</strong>
+          <p>Posts</p>
+        </div>
+
+        <div>
+          <strong>12k</strong>
+          <p>Seguidores</p>
+        </div>
+
+        <div>
+          <strong>530</strong>
+          <p>Siguiendo</p>
+        </div>
+
+      </div>
+
+      <button className="boton-perfil">
+        Editar perfil
+      </button>
+
     </aside>
   );
 }
